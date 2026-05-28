59547d7f7972887f4a85134edd8bd6aad0212408
Roni Nahum <roninahum13@gmail.com>
2026-05-28 12:06:05 +0300

    Implemented Linear Probing hash

--- ProbingHashTable.java
+++ ProbingHashTable.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.ArrayList;
@@ -9,8 +10,10 @@ public class ProbingHashTable<K, V> implements HashTable<K, V> {
     final private double maxLoadFactor;
     private int capacity;
     private HashFunctor<K> hashFunc;
-    private Element<K,V>[] table;
-
+    private Element<K, V>[] table;
+    private Element<K, V> deleted;
+    private int size;
+    private int k;
 
     /*
      * You should add additional private fields as needed.
@@ -22,28 +25,96 @@ public class ProbingHashTable<K, V> implements HashTable<K, V> {
         this.capacity = 1 << k;
         this.hashFunc = hashFactory.pickHash(k);
         this.table = new Element[capacity];
+        this.deleted = new Element<>(null, null);
+        this.size = 0;
+        this.k = k;
 
     }
-	
-	public ProbingHashTable(HashFactory<K> hashFactory) {
+
+    public ProbingHashTable(HashFactory<K> hashFactory) {
         this(hashFactory, DEFAULT_INIT_CAPACITY, DEFAULT_MAX_LOAD_FACTOR);
     }
 
     public V search(K key) {
-        throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+        int index = hashFunc.hash(key);
+        if (table[index] == null) {
+            return null;
+        }
+        if (table[index].key() != null && table[index].key().equals(key)) {
+            return table[index].satelliteData();
+        }
+        for (int i = index + 1; i != index; i = (i + 1) % capacity) {
+            if (table[i] == null) {
+                return null;
+            }
+            if (table[i].key() != null && table[i].key().equals(key)) {
+                return table[i].satelliteData();
+            }
+        }
+        return null;
     }
 
     public void insert(K key, V value) {
-        throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+        if (size == capacity) {
+            rehashing();
+        }
+        int index = hashFunc.hash(key);
+        int firstDelete = -1;
+        while (table[index] != null) {
+            if (table[index] == deleted && firstDelete == -1) {
+                firstDelete = index;
+            } else {
+                if (table[index].key().equals(key)) {
+                    table[index].setSatData(value);
+                    return;
+                }
+            }
+            index = (index + 1) % capacity;
+        }
+        Element<K, V> toInsert = new Element<>(key, value);
+        if (firstDelete != -1) {
+            table[firstDelete] = toInsert;
+            size++;
+        } else {
+            table[index] = toInsert;
+            size++;
+        }
     }
 
     public boolean delete(K key) {
-        throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+        int index = hashFunc.hash(key);
+        while (table[index] != null) {
+            if (table[index] != deleted && table[index].key().equals(key)) {
+                table[index] = deleted;
+                size--;
+                return true;
+            }
+            index = (index + 1) % capacity;
+        }
+        return false;
     }
 
     public HashFunctor<K> getHashFunc() {
         return hashFunc;
     }
 
-    public int capacity() { return capacity; }
+    public int capacity() {
+        return capacity;
+    }
+
+    public void rehashing() {
+        this.capacity = 2 * this.capacity;
+        Element<K, V>[] oldTable = table;
+        this.table = new Element[capacity];
+        this.k = k + 1;
+        this.hashFunc = hashFactory.pickHash(k);
+        this.size = 0;
+        for (int i = 0; i < oldTable.length; i++) {
+            if (oldTable[i] != null && oldTable[i] != deleted) {
+                Element<K, V> curr = oldTable[i];
+                insert(curr.key(), curr.satelliteData());
+            }
+        }
+    }
 }
+
