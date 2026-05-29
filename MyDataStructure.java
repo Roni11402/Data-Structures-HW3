b1675e4b8fc3e4b094d6c7bda53ed83fa5aac0db
Roni Nahum <roninahum13@gmail.com>
2026-05-29 10:21:53 +0300

    Implemented DS as requested

--- MyDataStructure.java
+++ MyDataStructure.java
@@ -1,45 +1,97 @@
+import com.sun.net.httpserver.Filter;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class MyDataStructure {
-    /*
-     * You may add any fields that you wish to add.
-     * Remember that all the data-structures you use must be YOUR implementations,
-     * except for the List and its implementation for the operation Range(low, high).
-     */
+    private ModularHash hashFactory;
+    private ChainedHashTable hashTable;
+    private IndexableSkipList skipList;
 
     /***
      * This function is the Init function described in Part 4.
      *
      * @param N The maximal number of items that may reside in the DS.
      */
-    public MyDataStructure(int N) {}
 
-    /*
-     * In the following functions,
-     * you should REMOVE the place-holder return statements.
-     */
+    //Initiating a new Hash Table for Θ(N) and a skipList for Θ(1)
+    //The "while" loop runs for Θlogn, and creating an array that is in size k*k is Θ(N) as required
+    public MyDataStructure(int N) {
+        this.hashFactory = new ModularHash();
+        int k = 0;
+        while ((1 << k) < N) {
+            k++;
+        }
+        this.hashTable = new ChainedHashTable(hashFactory, k, 2.0);
+        this.skipList = new IndexableSkipList(0.5);
+    }
+
+
+    //in this function, we will insert the values in to an Indexable skip list for an expected runtime
+    //of Θ(logn) expected, as learned in class
+    // We will also insert it into the Hash table for an expected runtime of Θ(1) expected as learned in
+    //class.
+    // for a quick search and keep pointers in the hash table to shorten access times.
     public boolean insert(int value) {
-        return false;
+        AbstractSkipList.SkipListNode inserted = this.skipList.insert(value);
+        if (inserted != null) {
+            this.hashTable.insert(value, inserted);
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
+    //in this function, we will first search the key in the hash table, which allows us to keep a runtime
+    //of Θ(1) expected in case val isn't in the DS, since search time in a hash table is Θ(1) expected.
+    // if it is, we will remove it from both DS for Θ(logn) expected, as learned in class.
     public boolean delete(int value) {
-        return false;
+        AbstractSkipList.SkipListNode toDelete = (AbstractSkipList.SkipListNode) hashTable.search(value);
+        if (toDelete == null) {
+            return false;
+        } else {
+            skipList.delete(toDelete);
+            hashTable.delete(value);
+            return true;
+        }
     }
 
+    //In this function we will perform a search in the hash table for Θ(1) expected, as learned in class
     public boolean contains(int value) {
-        return false;
+        if (hashTable.search(value) == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
+    //In this function we will use the rank function of the Indexable skip list to return the rank for
+    //Θ(logn) expected, as we learned in class
     public int rank(int value) {
-        return -1;
+        return skipList.rank(value);
     }
 
+    //In this function we will use the select function of the Indexable skip list to return the index for
+    //Θ(logn) expected, as we learned in class
     public int select(int index) {
-        return Integer.MIN_VALUE;
+        return skipList.select(index);
     }
 
+    //In this function we are checking to see if the low key is in the DS - if not, we will return null,
+    //for an expected runtime of Θ(1), searching in a hash table.
+    //If it is in the DS, We will go over all keys in ascending order until reaching the "high" key.
+    //Since the loop runs on all keys in "L", runtime will be Θ(|L|) expected.
     public List<Integer> range(int low, int high) {
-        return null;
+        AbstractSkipList.SkipListNode curr = (AbstractSkipList.SkipListNode) hashTable.search(low);
+        if (curr == null) {
+            return null;
+        }
+        List<Integer> L = new ArrayList<>();
+        while (curr.key() <= high) {
+            L.add(curr.key());
+            curr = skipList.successor(curr);
+        }
+        return L;
     }
 }
